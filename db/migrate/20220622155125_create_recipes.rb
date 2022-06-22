e4523942baefafbcec5fcafe44acfab057bbd9a7
Thi-Ponce <thi.pponce@gmail.com>
2022-06-22 13:03:53 -0300

    run migrations

--- db/migrate/20220622155125_create_recipes.rb
+++ db/migrate/20220622155125_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
     create_table :recipes do |t|
       t.string :name
       t.integer :preparation_time
-      t.interger :cooking_time
+      t.integer :cooking_time
       t.text :description
       t.boolean :public
 
