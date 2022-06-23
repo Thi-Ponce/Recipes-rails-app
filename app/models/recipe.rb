43e655da805a4110c5d8b0c179198cc9e65fcb96
Thi-Ponce <thi.pponce@gmail.com>
2022-06-23 19:05:43 -0300

    finish recipe page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ActiveRecord::Base
-    belongs_to :users, class_name: 'User'
-    has_many :recipe_foods, dependent: :destroy, class_name: 'Recipe-foods'
-  
-    validates :name, presence: true
-    validates :description, presence: true
-  end
+  belongs_to :users, class_name: 'User'
+  has_many :recipe_foods, dependent: :destroy, class_name: 'RecipeFood'
+
+  validates :name, presence: true
+  validates :description, presence: true
+end
