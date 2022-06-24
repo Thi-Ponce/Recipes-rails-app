997bdb73b35ffeba46c90e569720e2da424ed0a7
Thi-Ponce <thi.pponce@gmail.com>
2022-06-24 18:48:39 -0300

    implement table on shopping page

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -2,6 +2,12 @@ class ShoppingListController < ApplicationController
   def show
     @user = current_user
     @recipes = @user.recipes
+    @recipe_food = []
+    @recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        @recipe_food.push(recipe_food)
+      end
+    end
     @total_items = 0
     @total_price = 0
     @recipes.each do |items|
