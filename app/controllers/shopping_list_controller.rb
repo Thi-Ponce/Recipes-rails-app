2329d2892b772dfeafe598fb6d9573fb3dc252fa
Thi-Ponce <thi.pponce@gmail.com>
2022-06-24 18:53:37 -0300

    fix linter issues

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -11,13 +11,9 @@ class ShoppingListController < ApplicationController
     @total_items = 0
     @total_price = 0
     @recipes.each do |items|
-      items.recipe_foods.each do |item|
+      items.recipe_foods.includes(:food).each do |item|
         @total_items += item.quantity
-      end
-    end
-    @recipes.each do |food|
-      food.recipe_foods.includes(:food).each do |recipe_food|
-       @total_price += recipe_food.quantity * recipe_food.food.price
+        @total_price += item.quantity * item.food.price
       end
     end
   end
